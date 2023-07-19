f204d67b76f4fdce7ece5958e017964a2f73a304
Almuthanna-yahya <youremail@domain.com>
2023-07-18 22:25:28 -0400

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
 """
 #Module: translator
 This module provides a function for translating English text 
-to French using the MyMemoryTranslator API.
+to French using and French to English the MyMemoryTranslator API.
 """
 from deep_translator import MyMemoryTranslator
 
@@ -10,5 +10,13 @@ def english_to_french(english_text: str) -> str:
     using the MyMemoryTranslator API."""
     try:
         return MyMemoryTranslator(source="en", target="fr").translate(english_text)
+    except Exception as snake_case:
+        return f"Translation Error: {str(snake_case)}"
+
+def french_to_english(french_text: str) -> str:
+    """Translates French text to English 
+    using the MyMemoryTranslator API."""
+    try:
+        return MyMemoryTranslator(source="fr", target="en").translate(french_text)
     except Exception as snake_case:
         return f"Translation Error: {str(snake_case)}"
